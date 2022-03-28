d53b962a763b74144e7da2513d69e2abca19e6de
Gareth Williams <garethrwilliams@me.com>
2022-03-28 12:19:17 +0100

    nc-news test file added with test server test that passes

---
+++ __tests__/nc-news.test.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+const app = require('../app');
+const db = require('../db/connection');
+const seed = require('../db/seeds/seed');
+const testData = require('../db/data/test-data');
+
+beforeEach(() => seed(testData));
+afterAll(() => db.end());
+
+describe('testing server works', () => {
+  test('it works', () => {});
+});
