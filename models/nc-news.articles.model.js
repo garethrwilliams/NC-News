50df401acd6c5e2e95cb9b7e7f3e99d0507093de
garethrwilliams <75028060+garethrwilliams@users.noreply.github.com>
2022-05-19 13:25:46 +0100

    adds username to article response

--- models/nc-news.articles.model.js
+++ models/nc-news.articles.model.js
@@ -13,7 +13,7 @@ exports.selectArticles = async (
   const topicsArr = topics.rows.map((e) => e.topic);
 
   if (topic !== undefined && !topicsArr.includes(topic)) {
-    return Promise.reject({code: 404, error: 'Topic does not exist'});
+    return Promise.reject({ code: 404, error: 'Topic does not exist' });
   }
 
   if (
@@ -27,7 +27,7 @@ exports.selectArticles = async (
       'comment_count',
     ].includes(sort_by)
   ) {
-    return Promise.reject({code: 404, error: 'Sort_by field does not exist'});
+    return Promise.reject({ code: 404, error: 'Sort_by field does not exist' });
   }
 
   let i = 0;
@@ -56,7 +56,10 @@ exports.selectArticles = async (
   const articles = await db.query(sql, values);
 
   if (articles.rows.length === 0) {
-    return Promise.reject({code: 404, error: 'There are no further articles'});
+    return Promise.reject({
+      code: 404,
+      error: 'There are no further articles',
+    });
   }
 
   return articles.rows;
@@ -73,7 +76,7 @@ exports.selectArticleById = async (articleId) => {
   const article = await db.query(sql, [articleId]);
 
   if (article.rows.length === 0) {
-    return Promise.reject({code: 404, error: 'Article not found'});
+    return Promise.reject({ code: 404, error: 'Article not found' });
   }
 
   return article.rows[0];
@@ -94,7 +97,10 @@ exports.selectCommentsByArticleId = async (article_id, limit = 10, p = 1) => {
   const comments = await Promise.all([commentsDbQuery, articleDbQuery]);
 
   if (comments[0].rows.length === 0 && p > 1) {
-    return Promise.reject({code: 404, error: 'There are no further comments'});
+    return Promise.reject({
+      code: 404,
+      error: 'There are no further comments',
+    });
   }
 
   return comments[0].rows;
@@ -105,7 +111,7 @@ exports.updateArticleById = async (articleId, inc_votes) => {
 
   let votes = await db.query(sql1, [articleId]);
   if (votes.rows.length === 0) {
-    return Promise.reject({code: 404, error: 'Article not found'});
+    return Promise.reject({ code: 404, error: 'Article not found' });
   }
 
   votes = votes.rows[0];
@@ -124,7 +130,7 @@ exports.updateArticleById = async (articleId, inc_votes) => {
 };
 
 exports.insertArticle = async (new_article) => {
-  const {author, title, body, topic} = new_article;
+  const { author, title, body, topic } = new_article;
 
   if (!author || !title || !body || !topic) {
     return Promise.reject({
@@ -165,6 +171,6 @@ exports.removeArticleById = async (article_id) => {
   const deletedArticle = await db.query(sql, [article_id]);
 
   if (deletedArticle.rows.length === 0) {
-    return Promise.reject({code: 404, error: 'Article not found'});
+    return Promise.reject({ code: 404, error: 'Article not found' });
   }
 };
